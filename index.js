c63fcdc8cb544f88abf01229874c649c27b7be49
Adeyemi Arimoro <arimoro71@gmail.com>
2023-02-28 07:41:30 -0600

    Added solution to index.html

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main = document.querySelector('main#main');
+main.remove();
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Yemi is the champion";
+Element.append(newHeader);
+
+
+
+
+
